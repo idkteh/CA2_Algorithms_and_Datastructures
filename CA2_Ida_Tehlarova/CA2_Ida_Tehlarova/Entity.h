ba4f109f830b8e6343c6c75854ee9b5b6c931e3c
idkteh <124367770+idkteh@users.noreply.github.com>
2024-11-17 16:29:14 +0000

    constructors in entity + comments

--- CA2_Ida_Tehlarova/CA2_Ida_Tehlarova/Entity.h
+++ CA2_Ida_Tehlarova/CA2_Ida_Tehlarova/Entity.h
@@ -4,6 +4,17 @@ template <class K, class V>
 struct Entity {
 	K key;
 	V value;
+
+	Entity(K key, V value) {      // constructor for both key and value
+		this->key = key;
+		this->value = value;
+	}
+	Entity(K key) {      // constructor for only key, sometimes (like remove) only needs the key and no value
+		this->key = key;
+	}
+	Entity() {   // default constructor
+	}
+
 	bool operator<(Entity& other) {
 		return this->key < other.key;
 	}
--- CA2_Ida_Tehlarova/CA2_Ida_Tehlarova/TreeMap.h
+++ CA2_Ida_Tehlarova/CA2_Ida_Tehlarova/TreeMap.h
@@ -10,28 +10,53 @@ class TreeMap
 {
 	BinaryTree<Entity<K, V>> tree;
 public:
-	TreeMap();
 	void clear();
 	bool containsKey(K key);
 	int size();
 	V& get(K key);
 	BinaryTree<K> keySet();
 	void put(K key, V value);
-	Bool removeKey(K key);
+	bool removeKey(K key);
 	V& operator[K key];
-	~TreeMap();
 };
 
-template <class K,class V>
-TreeMap<K, V>::TreeMap() {
-
+template<class K, class V>
+inline void TreeMap<K, V>::clear()
+{
 
 }
 
+template<class K, class V>
+inline bool TreeMap<K, V>::containsKey(K key)
+{
+	return false;
+}
 
+template<class K, class V>
+inline int TreeMap<K, V>::size()
+{
+	return 0;
+}
 
+template<class K, class V>
+inline V& TreeMap<K, V>::get(K key)
+{
+	// TODO: insert return statement here
+}
 
-template <class K, class V>
-TreeMap<K, V>::~TreeMap() {
+template<class K, class V>
+inline BinaryTree<K> TreeMap<K, V>::keySet()
+{
+	return BinaryTree<K>();
+}
 
+template<class K, class V>
+inline void TreeMap<K, V>::put(K key, V value)
+{
+}
+
+template<class K, class V>
+inline bool TreeMap<K, V>::removeKey(K key)
+{
+	return bool();
 }
